a0db5dff0f4c067390140de80682ad30acd54908
Yahya <yahya.harrathi@gmail.com>
2017-02-09 17:16:43 +0100

    Adding a service
    service that takes data from a mock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,11 @@ import { HeroDetailComponent } from './hero-detail.component';
 @NgModule({
   imports: [BrowserModule,
     FormsModule],
+
   declarations: [AppComponent,
     HeroDetailComponent],
+
+    
   bootstrap: [AppComponent]
 })
 export class AppModule { }
