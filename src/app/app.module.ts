d85851ce8761aae221df01f1c44d3e0465d5e4d6
Yahya <yahya.harrathi@gmail.com>
2017-02-10 15:01:12 +0100

    Add Router to Heroes component
    Using router directive to navigate between views(Components)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+
+
 
 @NgModule({
-  imports: [BrowserModule,
-    FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
 
-  declarations: [AppComponent,
-    HeroDetailComponent],
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
 
-    
+      }
+
+    ])
+
+  ],
+
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent],
+
+  providers: [HeroService],
+
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
